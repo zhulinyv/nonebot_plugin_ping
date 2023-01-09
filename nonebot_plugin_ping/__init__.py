4a12403bcb0fc4cfc79b8e4ef4f9ae27fd350e1d
Xytpz <zhulinyv2005@outlook.com>
2023-01-09 18:45:11 +0800

    🐛更换 api 以支持海外服务器

--- nonebot_plugin_ping/__init__.py
+++ nonebot_plugin_ping/__init__.py
@@ -10,7 +10,7 @@ ping = on_command('ping', aliases={'Ping'}, priority=60, block=True)
 @ping.handle()
 async def _(msg: Message = CommandArg()):
     url = msg.extract_plain_text().strip()
-    api = f'https://api.juncikeji.xyz/api/ping.php?ip={url}'
+    api = f'https://api.gmit.vip/Api/Ping?format=json&ip={url}'
     message = await api_ping(api)
     await ping.finish(message)
 
@@ -18,12 +18,13 @@ async def api_ping(api):
     async with AsyncClient() as client:
             res = (await client.get(api)).json()
             if res["code"] == 200:
-                url = (res["data"]["域名"])
-                ip = (res["data"]["IP"])
-                max = (res["data"]["最大延迟"])
-                min = (res["data"]["最小延迟"])
-                place = (res["data"]["服务器归属地"])
-                res = f"域名: {url}\nIP: {ip}\n最大延迟: {max}\n最小延迟: {min}\n服务器归属地: {place}"
+                url = (res["data"]["host"])
+                ip = (res["data"]["ip"])
+                max = (res["data"]["ping_max"])
+                min = (res["data"]["ping_min"])
+                avg = (res["data"]["ping_avg"])
+                place = (res["data"]["location"])
+                res = f"域名: {url}\nIP: {ip}\n最大延迟: {max}\n最小延迟: {min}\n平均延迟: {avg}\n服务器归属地: {place}"
                 return res
             elif res["code"] == 201:
                 res = (res["data"])
